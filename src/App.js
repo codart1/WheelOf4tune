6ec4402097de56b9de4e52a6f45cfb8ad1b33ed5
codart1 <domanhcp@gmail.com>
2020-05-25 22:55:06 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Wheel from "Wheel";
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Wheel />
+    </div>
+  );
+}
